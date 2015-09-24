50b6b98518081fdd92fdb1de93c7633a4344418d
Chris Ward <cward@redhat.com>
2015-09-24 10:42:51 +0200

    load values from .clicktrack config

--- clicktrack/piwik.py
+++ clicktrack/piwik.py
@@ -8,8 +8,6 @@
 
 import datetime as dt
 import os
-import re
-import json
 
 import requests as rq
 from requests_kerberos import HTTPKerberosAuth, OPTIONAL
@@ -18,63 +16,64 @@ import yaml
 import warnings
 warnings.simplefilter('ignore')
 
+DEFAULT_CONFIG = os.path.expanduser('~/.clicktrack')
 TODAY = dt.date.today().strftime("%Y-%m-%d")
 YESTERDAY = (dt.date.today() - dt.timedelta(days=1)).strftime("%Y-%m-%d")
 
-def get_page_urls(filter_pattern, domain=None, site_id=None, date=(YESTERDAY, TODAY), period='range',
-                  data_format='json', limit=-1, expanded=True,
-                  filter_column='label', show_columns=None, total=False,
-                  hide_columns=None):
+
+def get_page_urls(filter_pattern=None, domain=None, idSite=None, date=None,
+                  period='range', format='json', filter_limit=-1, expanded=1,
+                  filter_column='label', showColumns=None, total=False,
+                  hideColumns=None, config=None):
     '''Docs...'''
+    config = os.path.expanduser(config or DEFAULT_CONFIG)
+    with open(config) as _:
+        config = yaml.load(_)
+
+    domain = domain or config.get('domain')
+    idSite = idSite or config.get('idSite')
     if domain is None:
         raise TypeError("Domain should be provided")
-    if site_id is None:
-        raise TypeError("SiteId should be provided")
+    if idSite is None:
+        raise TypeError("idSite should be provided")
+    period = period or config.get('period')
+    # expected as str YYYY-MM-DD,YYY-MM-DD
+    date = date or config.get('date', '{},{}'.format(YESTERDAY, TODAY))
+    expanded = int(expanded or config.get('expanded', 1))
+    filter_limit = int(filter_limit or config.get('filter_limit', -1))
+    filter_column = filter_column or config.get('filter_column', 'label')
+    filter_pattern = filter_pattern or config.get('filter_pattern')
+    showColumns = showColumns or config.get('showColumns')
 
     '''list all available columns in the documentation strings'''
-    payload = {'module': 'API',
-               'method': 'Actions.getPageUrls',
-               'format': data_format,
-               'idSite': site_id,
-               'period': period,
-               'date': '%s,%s' % date,
-               'expanded': int(expanded),
-               'filter_limit': limit,
-               'translateColumnNames': True,
-               'language': 'en',
-               'filter_column': filter_column,
-               'filter_pattern': filter_pattern,
-               'showColumns': ','.join(show_columns) if show_columns else None,
-               'hideColumns': ','.join(hide_columns) if hide_columns else None}
+    payload = {
+        'module': 'API',
+        'method': 'Actions.getPageUrls',
+        'format': format or config.get('format'),
+        'idSite': idSite,
+        'period': period,
+        'date': date,
+        'expanded': expanded,
+        'filter_limit': filter_limit,
+        'translateColumnNames': True,
+        'language': 'en',
+        'filter_column': filter_column,
+        'filter_pattern': filter_pattern,
+        # eg, ['label', 'nb_hits', 'nb_visits']
+        'showColumns': showColumns,
+        'hideColumns': hideColumns,
+    }
 
     auth = HTTPKerberosAuth(mutual_authentication=OPTIONAL)
-    data = rq.get(domain,
-                  params=payload,
-                  verify=False,
-                  auth=auth).text
+    resp = rq.get(domain, params=payload, verify=False, auth=auth)
 
-    if data_format == 'json':
-        data = json.loads(data)
-        # data = {key: [l[key] for l in data]for key in data[0].keys()}
+    if format == 'json':
+        data = resp.json()
+    else:
+        data = resp.text
 
-    if type(data) is dict and data["result"] == 'error':
+    if isinstance(data, dict) and data.get("result") == 'error':
         raise RuntimeError(
-            "You can't access this resource as it requires an 'view' access for the website id = %d" % (site_id))
+            "This resource requires 'view' access for idSite = %d" % (idSite))
 
     return data
-
-if __name__ == '__main__':
-    "Example"
-    CONFIG_FILE = os.path.expanduser('examples/config/piwik')
-    file_path = os.path.realpath(__file__)
-    dir_path = os.path.dirname(file_path)
-    repo_path = os.path.dirname(dir_path)
-
-    if os.path.join(repo_path, CONFIG_FILE):
-        with open(os.path.join(repo_path, CONFIG_FILE)) as _:
-            config = yaml.load(_)
-    else:
-        config = {}
-
-    print get_page_urls(filter_pattern=None, show_columns=['label', 'nb_hits', 'nb_visits'], **config)
-
